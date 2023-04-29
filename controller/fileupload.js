055c730e0ee370420c876febea9fa2815561e7eb
plusmail <plus4957@gmail.com>
2023-04-29 11:13:28 +0900

    upload storageMultifiles추가

--- controller/fileupload.js
+++ controller/fileupload.js
@@ -16,3 +16,24 @@ const storage = multer.diskStorage({
 
 exports.upload = multer({storage : storage});
 
+
+
+const storageMultifiles = multer.diskStorage({
+    destination: (req, file, callback) => {
+      callback(null, path.join(`${__dirname}/../../upload`));
+    },
+    filename: (req, file, callback) => {
+      const match = ["image/png", "image/jpeg"];
+  
+      if (match.indexOf(file.mimetype) === -1) {
+        var message = `<strong>${file.originalname}</strong> is invalid. Only accept png/jpeg.`;
+        return callback(message, null);
+      }
+  
+      var filename = `${Date.now()}-${file.originalname}`;
+      callback(null, filename);
+    }
+  });
+  
+  const uploadFiles = multer({ storage: storageMultifiles }).fields("multi-files", 10);
+  const uploadFilesMiddleware = util.promisify(uploadFiles);
